d748ea0040acff612aa90c5cbd9b8f49fa3de09f
ymikiii <1271311538@qq.com>
2021-11-01 21:15:55 +0800

    deploy

---
+++ precache-manifest.f63dfd19e5179e6155de0a74e3074671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fb8b4912ee8a5dcd73",
+    "url": "/morney-1-website/css/app.7f863453.css"
+  },
+  {
+    "revision": "318cb9109c2f4c3a754ad644d7041d8e",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "c7fb8b4912ee8a5dcd73",
+    "url": "/morney-1-website/js/app.fa4714ab.js"
+  },
+  {
+    "revision": "107a45754c27a3b0dee3",
+    "url": "/morney-1-website/js/chunk-vendors.05c43f1d.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
